8d236ec26dea5f222fd01efa58af6ce8b679a1d5
SIDDHANT BHATIA <siddhantbhatia220@gmail.com>
2025-02-05 12:13:06 +0530

    code.c

--- Print Multiplication Table@@676c4885c18c9c17a0f50668/code.c
+++ Print Multiplication Table@@676c4885c18c9c17a0f50668/code.c
@@ -1,5 +1,12 @@
-#include <stdio.h>
-int main() {
-    printf("Hello");
-    return 0;
+#include<stdio.h>
+int main()
+{
+	int a,i;
+	printf("Enter any number : ");
+	scanf("%d",&a);
+	for(i=1;i<=10;i++)
+	{
+		printf("%d x %d = %d\n", a,i,a*i);
+	}
+	return 0;
 }
